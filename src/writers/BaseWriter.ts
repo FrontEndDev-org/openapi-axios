cf9cb71ea4dd73feeb59f0f3c2128134e2a2cbd0
#云淡然 <hi@ydr.me>
2023-04-15 14:03:01 +0800

    feat(writer): 新增 axiosImport 选项

--- src/writers/BaseWriter.ts
+++ src/writers/BaseWriter.ts
@@ -1,9 +1,12 @@
 import { TypeAlias, TypeDocument, TypeItem } from '../readers/types';
+import { joinSlices } from '../utils/string';
 import { StrictWriterOptions, WriterOptions } from './types';
 import prettier from 'prettier';
 
 export class BaseWriter {
-  static defaults: WriterOptions = {
+  static defaults: StrictWriterOptions = {
+    axiosImport: `import { Axios } from 'axios';
+const axios = new Axios();`,
     prettier: {
       singleQuote: true,
     },
@@ -16,6 +19,11 @@ export class BaseWriter {
   options: StrictWriterOptions;
   constructor(readonly document: TypeDocument, options?: WriterOptions) {
     this.options = Object.assign({}, BaseWriter.defaults, options) as StrictWriterOptions;
+    this.init();
+  }
+
+  init() {
+    //
   }
 
   protected isTypeAlias(type: TypeItem): type is TypeAlias {
@@ -27,5 +35,26 @@ export class BaseWriter {
       ...this.options.prettier,
       parser: 'typescript',
     });
+  }
+
+  // 头部声明
+  statements: string[] = [];
+
+  // 头部导入
+  imports: string[] = [];
+
+  // 帮助类型
+  helpers: string[] = [];
+
+  writeStatements() {
+    return joinSlices(this.statements);
+  }
+
+  writeImports() {
+    return this.format(joinSlices(this.imports));
+  }
+
+  writeHelpers() {
+    return this.format(joinSlices(this.helpers));
   }
 }
