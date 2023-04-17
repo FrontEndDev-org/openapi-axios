cc81d121845df0952cc9a578a6e3a3d2498d6b7c
阿森 <zhenxs2018@foxmail.com>
2023-04-17 12:30:18 +0800

    build: switch rollup to vite

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { createRequire } from 'node:module';
+import externals from 'rollup-plugin-node-externals';
+
+import dts from 'vite-plugin-dts';
+// https://cn.vitest.dev/config/
+import { defineConfig } from 'vitest/config';
+
+const require = createRequire(import.meta.url);
+
+const pkg = require('./package.json');
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+    {
+      ...externals(),
+      enforce: 'pre',
+    },
+  ],
+  define: {
+    'process.env.PKG_NAME': JSON.stringify(pkg.name),
+    'process.env.PKG_VERSION': JSON.stringify(pkg.version),
+  },
+  build: {
+    lib: {
+      entry: ['src/index.ts', 'src/helpers.ts'],
+      formats: ['es', 'cjs'],
+    },
+    modulePreload: false,
+    sourcemap: false,
+    minify: false,
+    copyPublicDir: false,
+    outDir: 'dist',
+  },
+  test: {
+    globals: true,
+    coverage: {
+      reporter: ['lcov', 'text'],
+      // 包含所有源文件的覆盖率，而不是仅被单测的部分
+      all: true,
+    },
+  },
+});
